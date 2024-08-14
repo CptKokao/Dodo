85862d8a7b7857ca6d88997ed310596204daf258
cpt.kokao <cpt.kokao@gmail.com>
2024-08-14 12:16:31 +0300

    [3] Добавляет фильтры

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Filters } from "@/shared/components/shared";
 import { Container } from "@/shared/components/shared/container";
 import { Title } from "@/shared/components/shared/title";
 import { TopBar } from "@/shared/components/shared/top-bar";
@@ -6,10 +7,26 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <Container className="mt-10">
-      <Title text="Все пиццы" className="font-extrabold" />
+    <>
+      <Container className="mt-10">
+        <Title text="Все пиццы" className="font-extrabold" />
+      </Container>
 
       <TopBar />
-    </Container>
+
+      <Container className="mt-10 pb-14">
+        <div className="flex gap-[80px]">
+          {/* Фильтрация */}
+          <div className="w-[250px]">
+            <Filters />
+          </div>
+
+          {/* Список товаров */}
+          <div className="flex-1">
+            <div className="flex flex-col gap-16"></div>
+          </div>
+        </div>
+      </Container>
+    </>
   );
 }
