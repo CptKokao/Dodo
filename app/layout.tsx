ced349f6a4cd650cdae3ed22756c40b03c1db9d5
cpt.kokao <cpt.kokao@gmail.com>
2024-10-28 13:35:52 +0300

    [21] Добавляет авторизацию/регистрацию

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import { Nunito } from 'next/font/google';
-import { Toaster } from 'react-hot-toast';
 
 import './globals.css';
+import { Providers } from '@/shared/components/shared';
 
 const nunito = Nunito({
 	subsets: ['cyrillic'],
@@ -19,8 +19,10 @@ export default function RootLayout({
 			<head>
 				<link data-rh='true' rel='icon' href='/logo.png' />
 			</head>
-			<Toaster />
-			<body className={nunito.className}>{children}</body>
+
+			<body className={nunito.className}>
+				<Providers>{children}</Providers>
+			</body>
 		</html>
 	);
 }
